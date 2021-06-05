b12ce8c1c3935d495afa0d30aeecaac12a85936d
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2021-06-05 01:21:05 -0300

    homepage add

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { FaBars, FaTimes } from "react-icons/fa";
+import { Button } from "../../globalStyles"
 import { IconContext } from "react-icons/lib"
 import { 
   Nav, 
@@ -9,13 +10,30 @@ import {
   MobileIcon,
   NavMenu,
   NavItem,
-  NavLinks
+  NavLinks,
+  NavItemBtn,
+  NavBtnLink
 } from './Navbar.elements';
 
 const Navbar = () => {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener('resize', showButton);
 
   return (
     <>
@@ -29,10 +47,29 @@ const Navbar = () => {
           <MobileIcon onClick={handleClick}>
             {click ? <FaTimes /> : <FaBars />}
           </MobileIcon>
-          <NavMenu onClick={handleClick}>
+          <NavMenu onClick={handleClick} click={click}>
             <NavItem>
               <NavLinks to="/">Home</NavLinks>
+            </NavItem>          
+            <NavItem>
+              <NavLinks to="/services">Services</NavLinks>
             </NavItem>
+            <NavItem>
+              <NavLinks to="/products">Products</NavLinks>
+            </NavItem>
+            <NavItemBtn>
+              {button ? (
+                <NavBtnLink to="/sign-up">
+                  <Button primary>SIGN UP</Button>
+                </NavBtnLink>
+              ) : (
+                <NavBtnLink to="/sign-up">
+                  <Button fontBig primary>
+                    SIGN UP
+                  </Button>
+                </NavBtnLink>
+              )}
+            </NavItemBtn>
           </NavMenu>
         </NavbarContainer>
       </Nav>
